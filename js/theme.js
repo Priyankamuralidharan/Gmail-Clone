5cc32f5ca9e0181de8f69c8aef4c6e38438a7aaa
Harikishanth R <harikishanth@gmail.com>
2026-07-05 14:18:06 +0530

    dardarx's commit

---
+++ js/theme.js
@@ -0,0 +1,14 @@
+jsdocument.getElementById('themeToggleBtn').addEventListener('click', toggleTheme);
+
+function toggleTheme() {
+  document.body.classList.toggle('dark-mode');
+  const isDark = document.body.classList.contains('dark-mode');
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+  }
+});
